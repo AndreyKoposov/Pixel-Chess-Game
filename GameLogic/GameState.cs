dbdd74967cebed9629404c98269cef9ff23b4a48
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2024-05-20 01:15:05 +0300

    Pieces and Board

---
+++ GameLogic/GameState.cs
@@ -0,0 +1,14 @@
+﻿namespace GameLogic
+{
+    public class GameState
+    {
+        public Board Board { get; }
+        public Player CurrentPlayer { get; private set; }
+
+        public GameState(Player nextPlayer, Board nextBoard)
+        {
+            CurrentPlayer = nextPlayer;
+            Board = nextBoard;
+        }
+    }
+}
