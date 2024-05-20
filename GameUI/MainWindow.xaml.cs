e97d6ba3fd83ae5d1a8434de3d91b1b0bb744bda
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2024-05-20 11:55:52 +0300

    UI Moves

--- GameUI/MainWindow.xaml.cs
+++ GameUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,14 +19,25 @@ namespace GameUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlights = new Rectangle[8, 8];
+        private readonly Dictionary<Position, Move> moveCache = [];
+
         private GameState gameState;
+        private Position selectedPos = null;
         public MainWindow()
         {
             InitializeComponent();
             InitializeBoard();
+            //SetCursor();
 
             gameState = new GameState(Player.White, Board.Initial());
             DrawBoard(gameState.Board);
+        }
+
+        private void SetCursor()
+        {
+            Stream stream = Application.GetResourceStream(new Uri("Assets/pixel chess_v1.2/cursor.cur", UriKind.Relative)).Stream;
+            Cursor = new Cursor(stream, true);
         }
 
         private void InitializeBoard()
@@ -37,6 +49,10 @@ namespace GameUI
                     Image image = new Image();
                     pieceImages[r, c] = image;
                     PiecesGrid.Children.Add(image);
+
+                    Rectangle rect = new Rectangle();
+                    highlights[r, c] = rect;
+                    HighlightGrid.Children.Add(rect);
                 }
             }
         }
@@ -50,6 +66,88 @@ namespace GameUI
                     Piece piece = board[r, c];
                     pieceImages[r, c].Source = Images.GetImage(piece);
                 }
+            }
+        }
+
+        private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = e.GetPosition(HighlightGrid);
+            Position pos = ToSquarePosition(point);
+
+            if(selectedPos == null)
+            {
+                OnFromPositionSelected(pos);
+            }
+            else
+            {
+                OnToPositionSelected(pos);
+            }
+        }
+
+        private Position ToSquarePosition(Point point)
+        {
+            double squareSize = HighlightGrid.ActualWidth / 8;
+
+            int row = (int)(point.Y / squareSize);
+            int col = (int)(point.X / squareSize);
+
+            return new Position(row, col);
+        }
+
+        private void OnFromPositionSelected(Position pos)
+        {
+            IEnumerable<Move> moves = gameState.LegalMovesForPiece(pos);
+
+            if (moves.Any())
+            {
+                selectedPos = pos;
+                CacheMoves(moves);
+                ShowHighlights();
+            }
+        }
+
+        private void OnToPositionSelected(Position pos)
+        {
+            selectedPos = null;
+            HideHighlights();
+
+            if(moveCache.TryGetValue(pos, out Move move))
+            {
+                HandleMove(move);
+            }
+        }
+
+        private void HandleMove(Move move)
+        {
+            gameState.Move(move);
+            DrawBoard(gameState.Board);
+        }
+
+        private void CacheMoves(IEnumerable<Move> moves)
+        {
+            moveCache.Clear();
+
+            foreach(Move move in moves)
+            {
+                moveCache[move.ToPos] = move;
+            }
+        }
+
+        private void ShowHighlights()
+        {
+            Color color = Color.FromArgb(150, 255, 125, 125);
+
+            foreach(Position pos in moveCache.Keys)
+            {
+                highlights[pos.Row, pos.Column].Fill = new SolidColorBrush(color);
+            }
+        }
+
+        private void HideHighlights()
+        {
+            foreach (Position pos in moveCache.Keys)
+            {
+                highlights[pos.Row, pos.Column].Fill = Brushes.Transparent;
             }
         }
     }
