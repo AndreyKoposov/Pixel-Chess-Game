ca7da4265d661c91608cb3eccc2d776e077bc286
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2024-05-21 17:11:50 +0300

    Damage by area

--- GameLogic/Moves/ShotMove.cs
+++ GameLogic/Moves/ShotMove.cs
@@ -1,4 +1,6 @@
-﻿namespace GameLogic
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace GameLogic
 {
     internal class ShotMove : Move
     {
@@ -24,25 +26,87 @@
 
         public void MakeShot(Board board)
         {
-            Piece piece = board[ToPos];
             GunKing gunKing = (GunKing)board[FromPos];
 
-            int damage = 1;
-            double distance = Math.Sqrt(Math.Pow((ToPos.Row - FromPos.Row), 2) + Math.Pow((ToPos.Column - FromPos.Column), 2));
+            double distance = GetDistance(FromPos, ToPos);
+            int damage = GetDamageByDistance(distance);
+
+            List<Position> area = GetShotArea(distance, GetShotDirection(FromPos, ToPos));
+
+            foreach(Position pos in area)
+            {
+                if (!Board.IsInside(pos) || board.IsEmpty(pos)) continue;
+
+                Piece piece = board[pos];
+                piece.HP -= damage;
+                if (piece.HP <= 0)
+                {
+                    board[pos] = null;
+                }
+            }
+
+            gunKing.Bullets--;
+            gunKing.HasShoot = true;
+        }
+
+        private double GetDistance(Position FromPos, Position ToPos)
+        {
+            double delta_row = ToPos.Row - FromPos.Row;
+            double delta_col = ToPos.Column - FromPos.Column;
+
+            return Math.Sqrt(Math.Pow(delta_row, 2) + Math.Pow(delta_col, 2));
+        }
+
+        private int GetDamageByDistance(double distance)
+        {
+            int damage = 0;
 
             if (distance < 2) damage = 4;
             else if (distance < 4 && distance >= 2) damage = 3;
             else if (distance < 6 && distance >= 4) damage = 2;
             else if (distance >= 6) damage = 1;
 
-            piece.HP -= damage;
-            if (piece.HP <= 0)
+            return damage;
+        }
+
+        private List<Position> GetShotArea(double distance, Direction dir)
+        {
+            Position[] area = new Position[GetAreaSizeByDistance(distance)];
+
+            int delta = -(area.Length / 2);
+            for (int i = 0; i < area.Length; i++)
             {
-                board[ToPos] = null;
+                Position nextPos = new Position(ToPos.Row + delta * dir.RowDelta, ToPos.Column + delta * dir.ColumnDelta);
+                area[i] = nextPos;
+                delta++;
             }
 
-            gunKing.Bullets--;
-            gunKing.HasShoot = true;
+            return new List<Position>(area);
+        }
+
+        private int GetAreaSizeByDistance(double distance)
+        {
+            int size = 0;
+
+            if (distance < 2) size = 1;
+            else if (distance < 4 && distance >= 2) size = 1;
+            else if (distance < 6 && distance >= 4) size = 3;
+            else if (distance >= 6) size = 5;
+
+            return size;
+        }
+
+        private Direction GetShotDirection(Position FromPos, Position ToPos)
+        {
+            Direction dir;
+
+            double delta_row = ToPos.Row - FromPos.Row;
+            double delta_col = ToPos.Column - FromPos.Column;
+
+            if (delta_row <  delta_col) dir = new Direction(0, Math.Sign(delta_row));
+            else                        dir = new Direction(Math.Sign(delta_col), 0);
+
+            return dir;
         }
     }
 }
