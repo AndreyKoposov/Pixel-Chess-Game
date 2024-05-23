92d0e8bc1d6b45886bd13ea66ccb269ff4a33b94
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2024-05-24 00:38:43 +0300

    Add King to level 1

--- GameLogic/LevelBuilders/Level 1 Builder.cs
+++ GameLogic/LevelBuilders/Level 1 Builder.cs
@@ -3,6 +3,10 @@
 internal class Level1Builder : ILevelBuilder {
     public Level1Builder (Board board) : base(board) { }
 
+    internal override void AddKings()
+    {
+        board[0, 4] = new King(Player.Black);
+    }
     internal override void AddPawns () {
         for (int i = 0; i < 8; i++) {
             board[1, i] = new Pawn(Player.Black);
